219321368d05492c602719301e7f52e199cb2e43
Bikram <bdasgupta@gmail.com>
2021-03-23 17:38:44 +0800

    changed main to check .gitignore

--- EasyAppointmentSystem/EasyAppointmentSystemClient/src/java/easyappointmentsystemclient/Main.java
+++ EasyAppointmentSystem/EasyAppointmentSystemClient/src/java/easyappointmentsystemclient/Main.java
@@ -16,7 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("sup");
+        System.out.println("test");
     }
     
 }
