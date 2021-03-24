799a747a9cbb7826831efe7842bbfb3362430365
bdasgupta02 <bdasgupta02@gmail.com>
2021-03-24 19:39:42 +0800

    added Category entity

---
+++ EasyAppointmentSystem/EasyAppointmentSystemLibrary/src/entity/CategoryEntity.java
@@ -0,0 +1,69 @@
+package entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class CategoryEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long categoryId;
+    @Column(nullable=false, unique=true)
+    private String category;
+
+    public CategoryEntity(String category) {
+        this();
+        this.category = category;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public CategoryEntity() {
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (categoryId != null ? categoryId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the categoryId fields are not set
+        if (!(object instanceof CategoryEntity)) {
+            return false;
+        }
+        CategoryEntity other = (CategoryEntity) object;
+        if ((this.categoryId == null && other.categoryId != null) || (this.categoryId != null && !this.categoryId.equals(other.categoryId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.CategoryEntity[ id=" + categoryId + " ]";
+    }
+    
+}
