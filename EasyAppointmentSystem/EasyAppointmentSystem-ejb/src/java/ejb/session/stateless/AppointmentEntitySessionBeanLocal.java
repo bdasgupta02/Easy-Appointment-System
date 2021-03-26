be3ca68a5dbc1971d3affa137c9715ae15d40f41
bdasgupta02 <bdasgupta02@gmail.com>
2021-03-26 20:09:58 +0800

    Created appointment session bean with CRUD operations and local/remote refs

--- EasyAppointmentSystem/EasyAppointmentSystem-ejb/src/java/ejb/session/stateless/AppointmentEntitySessionBeanLocal.java
+++ EasyAppointmentSystem/EasyAppointmentSystem-ejb/src/java/ejb/session/stateless/AppointmentEntitySessionBeanLocal.java
@@ -5,8 +5,19 @@
  */
 package ejb.session.stateless;
 
+import entity.AppointmentEntity;
 import javax.ejb.Local;
+import util.exception.AppointmentNotFoundException;
+import util.exception.EntityAttributeNullException;
 
 public interface AppointmentEntitySessionBeanLocal {
+
+    public Long createAppointmentEntity(AppointmentEntity newAppointmentEntity) throws EntityAttributeNullException;
+
+    public AppointmentEntity retrieveAppointmentEntityById(Long appointmentId) throws AppointmentNotFoundException;
+
+    public void updateAppointmentEntity(AppointmentEntity appointmentEntity) throws EntityAttributeNullException;
+
+    public void deleteAppointmentEntity(Long appointmentId) throws AppointmentNotFoundException;
     
 }
--- EasyAppointmentSystem/EasyAppointmentSystemLibrary/src/ejb/session/stateless/AppointmentEntitySessionBeanRemote.java
+++ EasyAppointmentSystem/EasyAppointmentSystemLibrary/src/ejb/session/stateless/AppointmentEntitySessionBeanRemote.java
@@ -5,6 +5,18 @@
  */
 package ejb.session.stateless;
 
+import entity.AppointmentEntity;
+import util.exception.AppointmentNotFoundException;
+import util.exception.EntityAttributeNullException;
+
 public interface AppointmentEntitySessionBeanRemote {
+    
+    public Long createAppointmentEntity(AppointmentEntity newAppointmentEntity) throws EntityAttributeNullException;
+
+    public AppointmentEntity retrieveAppointmentEntityById(Long appointmentId) throws AppointmentNotFoundException;
+
+    public void updateAppointmentEntity(AppointmentEntity appointmentEntity) throws EntityAttributeNullException;
+
+    public void deleteAppointmentEntity(Long appointmentId) throws AppointmentNotFoundException;
     
 }
